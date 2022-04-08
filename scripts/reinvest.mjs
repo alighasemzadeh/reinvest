682bd968f8132d17e5e649dfd60daca2387c1d27
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-04-08 11:17:49 +0430

    Add CosmosDirectory.mjs

--- scripts/reinvest.mjs
+++ scripts/reinvest.mjs
@@ -1,11 +1,10 @@
-import { Cosmos } from "../src/index.js";
-import message from "@cosmostation/cosmosjs/src/messages/proto";
+import 'dotenv/config'
+import CosmosDirectory from '../src/utils/CosmosDirectory.mjs';
 
-const mnemonic = process.env.MNEMONIC
-const chainId = "cosmoshub-4";
-const cosmos = new Cosmos(lcdUrl, chainId);
+const directory = CosmosDirectory();
 
-cosmos.setPath("m/44'/118'/0'/0/0");
-const address = cosmos.getAddress(mnemonic);
+const rpcUrl = directory.rpcUrl('chihuahua');
+const restUrl = directory.restUrl('chihuahua');
 
-console.log(address);
+console.log(restUrl);
+console.log(rpcUrl);
